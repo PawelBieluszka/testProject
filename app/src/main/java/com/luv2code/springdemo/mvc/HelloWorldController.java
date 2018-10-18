f75f933109a4f265ec8af7da2edc4a68d648222d
Paweł Biełuszka <p.bieluszka@gmail.com>
2018-10-18 17:08:28 +0200

    My change

--- app/src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ app/src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -31,7 +31,7 @@ public class HelloWorldController {
 		// convert the data to all caps
 		theName = theName.toUpperCase();
 		// create the message 
-		String result = "Yo! " + theName;
+		String result = "YoYo! " + theName;
 	//  add message to the model
 	    model.addAttribute("message", result);
 		return "helloworld";	
